dde6e58cd9cae2605d896af52c2eb32c3ef67446
John Bendfeldt <jfbendfeldt@gmail.com>
2017-04-25 14:21:37 -0500

    adding flair

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,8 @@
-
+// Variables that are used, sets up score and loses categories
 var words, picList, blank, wrongGuess, guessRemain, currentWord, score = 0, loses = 0, wordPic, audioWin;
 
 setUp ();
-
+// Sets up inital game status/ round reset basically
 function setUp () {
 
 var words= ["trump", "america", "freedom", "liberty", "justice", "flag", "eagle"];
@@ -16,35 +16,39 @@ var words= ["trump", "america", "freedom", "liberty", "justice", "flag", "eagle"
     audioWin = new Audio("assets/sounds/djwin.mp3");
     audioLoss = new Audio("assets/sounds/madworldloss.mp3");
 for (var i = 0; i < currentWord.length; i++) {
+    // Establishes blanks for letters
         blank += "_" + "";
     }
 };
 document.onkeyup = function(event) {
-
+// Establishes events on key stroke
     var userGuess = event.key;
     document.getElementById("blanks").textContent = blank;
     document.getElementById("wrongGuesses").textContent = wrongGuess;
     document.getElementById("status").textContent = "Current Word:";
-
+// Checks if guess was accurate
     checkGuess(userGuess);
     function checkGuess(userGuess) {
         if (currentWord.indexOf(userGuess) > -1) {
             for (var i = 0; i < currentWord.length; i++) { 
                 if (currentWord[i] === userGuess) {
+                    // Shows user guess with displayLtrAt function
                     displayLtrAt(userGuess, i);
                     endGame ();
              }
         }
+        // If the guess is wrong it is pushed down into the wrongGuess text area
         } else if (wrongGuess.indexOf(userGuess) === -1) {
             wrongGuess.push(userGuess);
             document.getElementById("wrongGuesses").textContent = wrongGuess;
+            // Subtracts 1 remaining guess
             guessRemain--;
             document.getElementById("remain").textContent = guessRemain;
             endGame ();
         } 
     }
 };
-
+// Puts the user guess in where a blank would be
 function displayLtrAt(letter, index) {
     var letterGuess = "";
     for (var i = 0; i < blank.length; i++) 
@@ -53,17 +57,18 @@ function displayLtrAt(letter, index) {
         } else {
             letterGuess += blank[i];
         }
+    // Changes guess to upper case
     blank = letterGuess.toUpperCase();
 
         document.getElementById("blanks").textContent = blank;
     };
-
-
+// When a round ends
 function endGame () {
 // Out of gueesses
     if (guessRemain === 0) {
         document.getElementById("picChange").src = ("assets/images/pepe-lost.jpg");
         document.getElementById("status").textContent = "You lose!";
+        // Plays losing clip
         audioLoss.play();
         document.getElementById("blanks").textContent = "Press key to restart";
         loses++;
@@ -74,6 +79,7 @@ function endGame () {
 // Guessed the word correctly
     else if (blank.indexOf("_") === -1 && guessRemain != 0) {
         document.getElementById("status").textContent = "You win!";
+        // Plays winning clip
         audioWin.play();
         document.getElementById("blanks").textContent = "Press any key to play again!";
         document.getElementById("picChange").src = wordPic;
