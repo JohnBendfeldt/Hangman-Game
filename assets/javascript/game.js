af1cc900b89025b0562a7b07eaa2d83d01b0d25e
John Bendfeldt <jfbendfeldt@gmail.com>
2017-04-27 20:59:07 -0500

    made the game fun and nonrepeating

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,28 +1,42 @@
 // Variables that are used, sets up score and loses categories
 var words, picList, blank, wrongGuess, guessRemain, currentWord, score = 0, loses = 0, wordPic, audioWin, audioLoss;
-
+var activeList = ["trump", "america", "freedom", "liberty", "justice", "flag", "eagle", "equality", "capitalism", "love", "constitution", "independence"];
 setUp();
 // Sets up inital game status/ round reset basically
 function setUp() {
-
-var words = ["trump", "america", "freedom", "liberty", "justice", "flag", "eagle", "equality", "capitalism", "love", "constitution", "independence"];
-    picList = ["assets/images/trump.jpg", "assets/images/america-2.jpg", "assets/images/freedom.jpg", "assets/images/liberty.jpg", "assets/images/justice.jpg", "assets/images/flag.jpg", "assets/images/eagle.jpg", "assets/images/equality.png", "assets/images/capitalism.jpg", "assets/images/love.gif", "assets/images/constitution.jpg", "assets/images/independence.jpg"];
-    ranNum = Math.floor(Math.random() * words.length);
-    currentWord = words[ranNum];
-    wordPic = picList[ranNum];
-    wrongGuess = [];
-    guessRemain = 10;
-    blank = "";
-    audioWin = new Audio("assets/sounds/djwin.mp3");
-    audioLoss = new Audio("assets/sounds/madworldloss.mp3");
-for (var i = 0; i < currentWord.length; i++) {
-    // Establishes blanks for letters
-        blank += "_" + "";
+    console.log(activeList)
+    var words = ["trump", "america", "freedom", "liberty", "justice", "flag", "eagle", "equality", "capitalism", "love", "constitution", "independence"];
+        picList = {trump: "assets/images/trump.jpg", america: "assets/images/america-2.jpg", freedom: "assets/images/freedom.jpg", 
+                   liberty: "assets/images/liberty.jpg", justice: "assets/images/justice.jpg", flag: "assets/images/flag.jpg", 
+                   eagle: "assets/images/eagle.jpg", equality: "assets/images/equality.png", capitalism: "assets/images/capitalism.jpg", 
+                   love: "assets/images/love.gif", constitution: "assets/images/constitution.jpg", independence: "assets/images/independence.jpg"};
+        ranNum = Math.floor(Math.random() * activeList.length);
+        currentWord = activeList[ranNum];
+        index = activeList.indexOf(currentWord);
+        activeList.splice(index, 1)
+        wordPic = picList[currentWord];
+        wrongGuess = [];
+        guessRemain = 10;
+        blank = "";
+        audioWin = new Audio("assets/sounds/djwin.mp3");
+        audioLoss = new Audio("assets/sounds/madworldloss.mp3");
+        availableLetters = "abcdefghijklmnopqrstuvwxyz";
+    if (activeList.length ==  0) {
+        activeList = words
+    }
+    for (var i = 0; i < currentWord.length; i++) {
+        // Establishes blanks for letters
+            blank += "_" + "";
     }
 };
+
 document.onkeyup = function(event) {
 // Establishes events on key stroke
     var userGuess = event.key;
+    if (availableLetters.indexOf(userGuess) == -1){
+        document.getElementById("status").textContent = "Please type a valid letter";
+        return // end and do nothing
+    };
     document.getElementById("blanks").textContent = blank;
     document.getElementById("wrongGuesses").textContent = wrongGuess;
     document.getElementById("status").textContent = "Current Word:";
